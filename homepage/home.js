df185977628baae6fac9549f5a70d79e380be836
Your Name <you@example.com>
2024-09-19 22:53:11 +0600

    got add money and pin from input

--- homepage/home.js
+++ homepage/home.js
@@ -1,1 +1,11 @@
-alert('fuck')
+// add money to a account
+// step-1: add an event handler to the add money button inside the form
+document.getElementById('btn-add-money').addEventListener('click', function(event) {
+    // prevent reload after form submit
+event.preventDefault();
+// step-2: get money to be added to an account
+const addMoneyInput = document.getElementById('add-money-input').value;
+// get the pin number provided
+const pinNumberInput = document.getElementById('secret-pin').value;
+
+})
