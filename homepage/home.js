7e42b987c3c0560f7c8ad630d778fff71d6baab0
Your Name <you@example.com>
2024-09-19 23:32:51 +0600

    Account balance update

--- homepage/home.js
+++ homepage/home.js
@@ -7,5 +7,18 @@ event.preventDefault();
 const addMoneyInput = document.getElementById('add-money-input').value;
 // get the pin number provided
 const pinNumberInput = document.getElementById('secret-pin').value;
+// step-3: verify the pin
+if(pinNumberInput === '1234') {
+    console.log('Money added');
+    // step-4: get the current balance
+    const currentBalance = document.getElementById('account-balance').innerText;
+    // step-5: add the addMoney with currentBalance
+    const updatedBalance = parseFloat(currentBalance) + parseFloat(addMoneyInput);
+    // step -6: UPdate the balance in the ui/dom
+    document.getElementById('account-balance').innerText = updatedBalance;
+}
+else {
+    alert('Wrong pin! Try again later')
+}
 
 })
